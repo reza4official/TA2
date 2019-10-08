7a99d0f081bfd83c9a7445a6c6f607539c850f0b
Moch. Rheza Wira Z. Hasbi <reza4official@gmail.com>
2019-10-08 15:47:33 +0700

    Kodingan lama
    Hanya sebagai referensi

--- test_pir/test_pir.ino
+++ test_pir/test_pir.ino
@@ -28,11 +28,12 @@
 
 #define RELAY_ON LOW // Relay LED off when relay on
 #define RELAY_OFF HIGH // Relay LED on when relay off
-#define RELAY_1  2   // pin yang digunakan bisa diganti ganti bro
+// #define RELAY_1  2   // pin yang digunakan bisa diganti ganti bro
 
 int indikator = 12; // buat indikator LED
 int inputVout = 13; // Vout PIR
 int statusPIR = LOW; // status logical
+int RELAY_1 = 2;
 int data = 0; // variabel temporary utk menampung data PIR 
  
 void setup() {
@@ -51,11 +52,10 @@ if ((data == HIGH) && (statusPIR == LOW)) { // cek jika ada pergerakan
        digitalWrite(indikator, LOW); // nyalakan led indikator di board Arduino
         Serial.println("GERAKAN TERDETEKSI!"); //buat monitor ke laptop
          statusPIR = HIGH; //diset high spy tdk mendeteksi terus
-         
-         digitalWrite(RELAY_1, RELAY_ON);
-         delay(10000);
-         digitalWrite(RELAY_1, RELAY_OFF);
-         delay(0);
+       digitalWrite(RELAY_1, RELAY_ON);
+       delay(10000);
+       digitalWrite(RELAY_1, RELAY_OFF);
+       delay(0);
 } else {
   if ((data == LOW) && (statusPIR == HIGH)){
    digitalWrite(indikator, HIGH); // matikan led indikator

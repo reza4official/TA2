a86a24c87aafbf4cd13b6ccc04c3becf21aff9a8
Moch. Rheza Wira Z. Hasbi <reza4official@gmail.com>
2019-11-11 13:53:56 +0700

    Kodingan layer 1, untuk layer 2 dan 3 perbedaan hanya di baris 40 dan 41 untuk membaca data firebase

--- pir_nodemcu/pir_nodemcu.ino
+++ pir_nodemcu/pir_nodemcu.ino
@@ -1,31 +1,45 @@
 #include <ESP8266WiFi.h>
 #include <FirebaseArduino.h>
 
-#define FIREBASE_HOST "light-control-fe6f8.firebaseio.com"                       //Your Firebase Project URL goes here without "http:" , "\" and "/"
-#define FIREBASE_AUTH "VrkBz0o2te1yZFSy7AaoHKMJcbtFLKsd832G0T9y"        //Your Firebase Database Secret goes here
+#define FIREBASE_HOST "tugasakhir-02.firebaseio.com"                    //Your Firebase Project URL goes here without "http:" , "\" and "/"
+#define FIREBASE_AUTH "6mh1TWAGMVB58xC4S1gIt9I94zdSNNp3OjqXCHvl"        //Your Firebase Database Secret goes here
 #define WIFI_SSID "Razer WiFi"                                          //Your WiFi Name
 #define WIFI_PASSWORD "01454765"                                        //Password of your wifi network 
+#define timeSeconds 20
 
 int relay = 2; // choose the pin for the RELAY
 int pir = 13; // choose the input pin (for PIR sensor)
 int gerak = 0; // variable for reading the pin status
-int bypass;
-int automode;
-int manualmode;
+int bypass, automode, manualmode, layer2, layer3; // variable for reading firebase value
+int layer1 = 1;
+int deteksigerak;
+
+unsigned long now = millis();
+unsigned long lastTrigger = 0;
+bool mulaiTimer = false;
 
 void setup() {
   pinMode(relay, OUTPUT); // declare lamp as output
-  pinMode(pir, INPUT); // declare sensor as input
+  pinMode(pir, INPUT_PULLUP); // declare sensor as input
+  attachInterrupt(digitalPinToInterrupt(pir), deteksiGerakan, RISING);
   digitalWrite(relay, 0);
   Serial.begin(9600);
+  Firebase.begin(FIREBASE_HOST,FIREBASE_AUTH);
+  Firebase.setInt("Layer 2", 0);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("connecting");
   while (WiFi.status()!=WL_CONNECTED){
     Serial.print(".");
     delay(500);
   }
   Serial.println();
-  Serial.print("connected:");
-  Firebase.begin(FIREBASE_HOST,FIREBASE_AUTH);
+  Serial.print("Connected to wifi");
+  bypass = Firebase.getString("Lampu1-1").toInt();
+  automode = Firebase.getString("Auto").toInt();
+  manualmode = Firebase.getString("Manual").toInt();
+  layer2 = Firebase.getString("Layer 2").toInt();
+  layer3 = Firebase.getString("Layer 3").toInt();
+  gerak = digitalRead(pir); // read input value
 }
 
 void firebasereconnect(){
@@ -33,75 +47,43 @@ void firebasereconnect(){
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 }
 
-//void PIRbypass(){
-//  lampu = Firebase.getString("Lampu").toInt();
-//  if (lampu1 == 0) {
-//    digitalWrite(relay, LOW); 
-//    Serial.println("Lampu Mati");
-//  }
-//  else {
-//    digitalWrite(relay,HIGH);
-//    Serial.println("Lampu Menyala");
-//  }
-//}
-
-  
+ICACHE_RAM_ATTR void deteksiGerakan(){
+  if (automode == 1 || (automode == 1 && manualmode == 0)){
+    digitalWrite(relay, HIGH);
+    Serial.println("Gerakan Terdeteksi!");
+    mulaiTimer = true;
+    lastTrigger = millis();
+    deteksigerak = 1;
+  }
+}
+ 
 void loop(){
+  now = millis();
+  if(layer3 == 1 || (layer2 == 0 && (mulaiTimer && (now - lastTrigger > (timeSeconds*1000))))) {
+//  if(layer3 == 1 || (mulaiTimer && (now - lastTrigger > (timeSeconds*1000))) {
+    digitalWrite(relay, LOW);
+    Serial.println("Gerakan Tidak Terdetksi");
+    mulaiTimer = false;
+    deteksigerak = 0;
+  }
+  if (deteksigerak == 1){
+    Firebase.setInt("Layer 2", 1);
+  } else {
+    Firebase.setInt("Layer 2", 0);
+  }
   if (Firebase.failed()){
     Serial.print("setting number failed:");
     Serial.println(Firebase.error());
     firebasereconnect();
     return;
   }
-  bypass = Firebase.getString("Lampu").toInt();
-  automode = Firebase.getString("Auto").toInt();
-  manualmode = Firebase.getString("Manual").toInt();
-  gerak = digitalRead(pir); // read input value
-  if (automode == 1){
-    if (gerak == 1){
-      digitalWrite(relay,HIGH); // turn ON the lamp
-      Serial.println("Gerakan Terdeteksi");
+  if (manualmode == 1 && automode == 0){
+      if (bypass == 0) {
+      digitalWrite(relay, LOW); 
+      Serial.println("Lampu1-1 Mati");
     } else {
-      digitalWrite(relay,LOW); // turn ON the lamp
-      Serial.println("Gerakan Tidak Terdeteksi");
+      digitalWrite(relay,HIGH);
+      Serial.println("Lampu1-1 Menyala");
     }
   }
-  if (manualmode == 1){
-    if (bypass == 0) {
-    digitalWrite(relay, LOW); 
-    Serial.println("Lampu Mati");
-  } else {
-    digitalWrite(relay,HIGH);
-    Serial.println("Lampu Menyala");
-  }
- }
 }
-//  if (lampu == 0) {
-//    digitalWrite (relay, LOW); 
-//    Serial.println("Lampu Mati");
-//  } else if (lampu == 1) {
-//    digitalWrite(relay,HIGH);
-//    Serial.println("Lampu Menyala");
-//  } else if (gerak == 1) {
-//    digitalWrite(relay,HIGH); // turn ON the lamp
-//    Serial.println("Gerakan Terdeteksi");
-//  } else {
-//    digitalWrite (relay,LOW); // turn OFF the lamp
-//    Serial.println("Gerakan Tidak Terdeteksi");
-//  }
-//  delay(1000);
-//  gerak = digitalRead(pir); // read input value
-//  if( gerak== 1) {
-//    PIRbypass();
-//    digitalWrite(relay,HIGH); // turn ON the lamp
-//    Serial.println("Gerakan Terdeteksi");
-//    delay(10000);
-//    Firebase.setInt("Lampu1",1);
-//  } else {
-//    PIRbypass();
-//    Firebase.setInt("Lampu1",0);
-//    digitalWrite(relay,LOW); // turn OFF the lamp
-//    Serial.println("Gerakan Tidak Terdeteksi");
-//    delay(0);
-//  }
-//}

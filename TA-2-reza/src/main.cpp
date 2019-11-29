d14da605f380eb2537d904edc3ddb518084f6b5e
Moch. Rheza Wira Z. Hasbi <reza4official@gmail.com>
2019-11-30 02:39:29 +0700

    add field to main

--- TA-2-reza/src/main.cpp
+++ TA-2-reza/src/main.cpp
@@ -1,100 +1,25 @@
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
-#include <FirebaseArduino.h>
 #include <Arduino.h>
+#include <Reader.h>
+#include <Firebase.h>
+#include <Actuator.h>
 
-#define FIREBASE_HOST "light-control-fe6f8.firebaseio.com"
-#define FIREBASE_AUTH "VrkBz0o2te1yZFSy7AaoHKMJcbtFLKsd832G0T9y"
-#define WIFI_SSID "Razer WiFi"
-#define WIFI_PASSWORD "01454765"
+Firebase firebase;
+Reader reader;
+Actuator actuator;
 
 /** constant */
-String nodeId = "light-001";
-int relayPin = 2;    // pin for the RELAY
-int pirPin = 13;     // input pin (for PIR sensor)
-int baud = 9600;     // input baud
-int loopDelay = 250; // milisecond
-
-/** declare inputs */
-bool motionDetected;
-bool sensorActive;
-bool isOverrided;
-
-/** log message to console */
-void log(String message)
-{
-  Serial.println("-- " + message + " --");
-}
-
-/** established connection to firebase */
-void firebaseConnect()
-{
-  log("connecting to firebase");
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-}
-
-/** get node state from firebase */
-void readNodeData()
-{
-  sensorActive = Firebase.getBool("nodes/" + nodeId + "/active");
-  isOverrided = Firebase.getBool("nodes/" + nodeId + "/bypass");
-}
-
-/** get sensor data */
-void readSensorData()
-{
-  int in = digitalRead(pirPin); // read from motion sensor
-  motionDetected = in == 1 ? true : false;
-  log(motionDetected ? "motion detected" : "motion not detected");
-}
-
-/** switch light */
-void toggleSwitch(bool isOn)
-{
-  digitalWrite(relayPin, isOn ? HIGH : LOW);
-}
+int baudRate = 9600; // input baud rate hz
 
 /** initiate prerequisite */
 void setup()
 {
-  /** set I/O */
-  pinMode(relayPin, OUTPUT); // declare relayPin as output
-  pinMode(pirPin, INPUT);    // declare pirPin as input
-  digitalWrite(relayPin, 0);
-
-  Serial.begin(baud);
-  Serial.print("connecting to baud " + baud);
-
-  /** wait until connected to wifi */
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.print("...");
-    delay(500);
-  }
-  Serial.print("connection established");
-
-  /** start connection to firebase instance */
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Serial.begin(baudRate);
+  // firebase.setupConnection(); // setup WiFi connection
+  // reader.setupSensor();
 }
 
 /** main loops */
 void loop()
 {
-  /** check firebase connection */
-  if (Firebase.failed())
-  {
-    log('firebase connection stalled');
-    firebaseConnect();
-    return;
-  }
-
-  /** get node and sensor data */
-  readNodeData();
-  readSensorData();
-
-  /** switch light on/off */
-  toggleSwitch(isOverrided || (sensorActive && motionDetected));
-
-  delay(loopDelay);
-  return;
+  firebase.maintainConnection();
 }
